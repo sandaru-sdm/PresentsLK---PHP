c744ac17460c544555a3163752aa7f11226bfa51
Sandaru Gunathilake <maduhansadilshan@gmail.com>
2024-11-26 10:59:40 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@ class Database{
     public static function setUpConnection(){
         if(!isset(Database::$connection)){
             Database::$connection = 
-            new mysqli("localhost","root","dilshan2000","presentlk","3307");
+            new mysqli("localhost","YOUR_DB_USERNAME","YOUR_DB_PASSWORD","presentlk","PORT");
         }
     }
 
